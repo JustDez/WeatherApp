803c3fbb94356aa13a54706d5abe17ea4811c220
Dev-Dez <134801129+Dev-Dez@users.noreply.github.com>
2023-12-05 09:08:10 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-days");
+
+
+const API_KEY = "c2dfc1de768a81b1a1bf43874001daee";
+
